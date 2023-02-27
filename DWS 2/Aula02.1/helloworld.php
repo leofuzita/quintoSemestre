8f04b783b470dcd364ada2f1e27071e000c2ce60
Dimas Ferreira <dimaslimaferreira@gmail.com>
2023-02-27 19:34:52 -0300

    Aulas

---
+++ DWS 2/Aula02.1/helloworld.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeira pagina PHP</title>
+</head>
+<body>
+    <h1>Minha primeira pagina PHP</h1> 
+
+    <?php
+        echo "Hello World!";
+    ?>
+    
+</body>
+</html>
